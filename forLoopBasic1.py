6d7cdb61c5044473c7b97a3d3369d3a8a496fd91
Rachel Peterson <rachelp5446@gmail.com>
2020-09-28 13:08:04 -0500

    Update existing files

--- forLoopBasic1.py
+++ forLoopBasic1.py
@@ -1,1 +1,48 @@
+# 1 - Basic
 
+for x in range(0,151,1):
+    print(x)
+
+
+# 2 - Multiples of Five
+
+for x in range(5,1001,5):
+    print(x)
+
+
+# 3 - Counting, the Dojo Way6
+
+for x in range(1,101,1):
+    if x % 10 == 0:
+        print('Coding Dojo')
+    elif x % 5 == 0:
+        print('Coding')
+    else:
+        print(x)
+        
+
+
+# 4 - Whoa. That Sucker's Huge
+
+sum = 0
+for x in range(0,500001,1):
+    if x % 2 != 0:
+        sum = sum + x
+print(sum)
+
+
+# 5 - Countdown by Fours
+
+for x in range(2018,0,-4):
+    print(x)
+
+
+# 6 - Flexible Counter
+
+lowNum = 2
+highNum = 9
+mult = 3
+
+for i in range(lowNum,highNum + 1,1):
+    if i % mult == 0:
+        print(i)
